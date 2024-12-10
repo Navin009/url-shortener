ca9b83ac0f792a0e88f808da7959f8946baa9b6e
Navin Kumar <mailbox.navinkumar@gmail.com>
2024-12-10 09:20:40 +0530

    Readme Changes

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -7,5 +7,6 @@ pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(ping::ping)
         .service(url::create_short_url)
         .service(url::redirect)
-        .service(url::get_url_details);
+        .service(url::get_url_details)
+        .service(ping::health);
 }
--- src/handlers/ping.rs
+++ src/handlers/ping.rs
@@ -1,6 +1,14 @@
 use actix_web::{get, HttpResponse};
+use serde_json::json;
 
 #[get("/ping")]
 pub async fn ping() -> HttpResponse {
     HttpResponse::Ok().json("pong")
+}
+
+#[get("/health")]
+pub async fn health() -> HttpResponse {
+    HttpResponse::Ok().json(json!({
+        "status": "UP"
+    }))
 }
